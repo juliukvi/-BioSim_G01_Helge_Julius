26a2aa2ce2a7e55357b96dc46909ac06cbc2bc4c
Helge <helge.helo@gmail.com>
2020-01-08 11:19:22 +0100

    Added files

---
+++ src/biosim/animals.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Helge Helo Klemetsdal'
+__email__ = 'hegkleme@nmbu.no'
---
+++ tests/test_landscape.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Helge Helo Klemetsdal'
+__email__ = 'hegkleme@nmbu.no'
+
